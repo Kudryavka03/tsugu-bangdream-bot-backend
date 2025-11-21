bc1427a3bea3cc710199222470e9f36057b32791
Kudryavka <Kudryavka03@outlook.com>
2025-11-21 22:56:56 +0800

    buffer编码方式优化

--- src/toBufferWorker.js
+++ src/toBufferWorker.js
@@ -1,23 +1,23 @@
-const { Canvas, Image, loadImage } = require('skia-canvas');
+const { Canvas, Image, loadImage,SkImage  } = require('skia-canvas');
 const { parentPort } = require('worker_threads');
 
 parentPort.on('message', async ({ id, width, height, pixels, format, quality }) => {
     try {
-        const canvas = new Canvas(width, height);
-        const ctx = canvas.getContext('2d');
+        //const canvas = new Canvas(width, height);
+        //const ctx = canvas.getContext('2d');
+        const img = SkImage.fromBytes(pixels, width, height);
+        //SkImage.toBuffer
+        //ctx.putImageData(img, 0, 0);
 
-        // 通过 raw buffer 创建 ImageData
-        const imageData = ctx.createImageData(width, height);
-
-        // 注意：skia-canvas 的 raw buffer 是 premultiplied alpha
-        // 直接 set 会显示正确
-        imageData.data.set(pixels);
-        ctx.putImageData(imageData, 0, 0);
-
+        /*
         // 输出 PNG/JPEG
         const buffer = format === 'jpeg'
             ? await canvas.toBuffer('jpeg', { quality })
             : await canvas.toBuffer('png');
+            */
+            const buffer = format === 'jpeg'
+            ? await img.toBuffer('jpeg', { quality })
+            : await img.toBuffer('png');
 
         parentPort.postMessage({ id, buffer }, [buffer.buffer]);
     } catch (e) {
