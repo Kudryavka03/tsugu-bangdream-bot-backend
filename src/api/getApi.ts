50a81e4ef809861f1ba0343c3f7a4fc10bf82f9d
Kudryavka <Kudryavka03@outlook.com>
2026-06-10 10:59:15 +0800

    fix: 偶现读status undefined的问题

--- src/api/getApi.ts
+++ src/api/getApi.ts
@@ -116,8 +116,8 @@ async function callAPIAndCacheResponseF(url: string, cacheTime: number = 0, retr
       
       logger(`API`, `Failed to get JSON from "${url}" on attempt ${attempt + 1}. Error: ${e.message}`);
       if (attempt === retryCount - 1) {
-        throw new Error(`Failed to get JSON from "${url}" after ${retryCount} attempts`);
-        //throw e; // Rethrow the error if all retries fail
+        logger(`API`,`Failed to get JSON from "${url}" after ${retryCount} attempts`);
+        throw e; // Rethrow the error if all retries fail
       }
       //等待3秒后重试
       await new Promise(resolve => setTimeout(resolve, 3000));
