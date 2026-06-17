8d2f7f5e9a1ed47e7e1fdfa41c562ccc68028a6c
Kudryavka <Kudryavka03@outlook.com>
2026-06-17 22:18:02 +0800

    fix: 多样化歌榜适配

--- src/view/cutoffSong.ts
+++ src/view/cutoffSong.ts
@@ -18,11 +18,15 @@ interface cutoffSongsResponse {
     result: boolean;
     cutoffs: Record<string, cutoffSongsDetail[]>;
 }
+interface cutoffSongsResponseVersus {
+    result: boolean;
+    cutoffs: cutoffSongsDetail[];
+}
 interface cutoffSongsDetail {
     time: number;
     ep: number;
 }
-
+const eventTypes: string[] = ['versus', 'challenge', 'medley'];
 export async function drawCutoffSongsDetail(eventId: number, tier: number, mainServer: Server, compress: boolean): Promise<Array<Buffer | string>> {
     const event = new Event(eventId);
     if (!event.isExist) {
@@ -31,7 +35,9 @@ export async function drawCutoffSongsDetail(eventId: number, tier: number, mainS
 
     await event.initFull();
 
-    const eventTypes: string[] = ['versus', 'challenge', 'medley'];
+    //const eventTypes: string[] = ['versus', 'challenge', 'medley'];
+    // versus只有一首歌
+    let isVersus = false
     if (!eventTypes.includes(event.eventType) || !event.musics || event.musics.length === 0) {
         return ['错误: 该活动不是歌榜活动或没有歌曲数据'];
     }
@@ -44,14 +50,23 @@ export async function drawCutoffSongsDetail(eventId: number, tier: number, mainS
     for (let i = 0; i < event.musics[defaultServer].length; i++) {
         songList.push(new Song(event.musics[defaultServer][i].musicId));
     }
+    console.log(songList)
 
     const SongTierUrl = `https://hhwx.org/api/bandori/tracker/data?server=${mainServer}&event=${eventId}&type=song&tier=${tier}`;
     const SongT1Url = `https://hhwx.org/api/bandori/tracker/data?server=${mainServer}&event=${eventId}&type=song&tier=1`;
+    if(event.eventType == 'versus'){
+        isVersus = true
+            var [vTier1, vTierN] = await Promise.all([
+            callAPIAndCacheResponse(SongT1Url, 0, 1, false, 2),
+            callAPIAndCacheResponse(SongTierUrl, 0, 1, false, 2)
+        ]) as [cutoffSongsResponseVersus, cutoffSongsResponseVersus];
+    }else{
+        var [tier1, tierN] = await Promise.all([
+            callAPIAndCacheResponse(SongT1Url, 0, 1, false, 2),
+            callAPIAndCacheResponse(SongTierUrl, 0, 1, false, 2)
+        ]) as [cutoffSongsResponse, cutoffSongsResponse];
+    }
 
-    const [tier1, tierN] = await Promise.all([
-        callAPIAndCacheResponse(SongT1Url, 0, 1, false, 2),
-        callAPIAndCacheResponse(SongTierUrl, 0, 1, false, 2)
-    ]) as [cutoffSongsResponse, cutoffSongsResponse];
 
     const t1Score = new Map<number, number>();
     const tierScore = new Map<number, number>();
@@ -59,14 +74,17 @@ export async function drawCutoffSongsDetail(eventId: number, tier: number, mainS
 
     for (const song of songList) {
         const songId = song.songId.toString();
-        if (!tier1.cutoffs[songId] || !tierN.cutoffs[songId]) {
+
+        if (!isVersus && (!tier1.cutoffs[songId] || !tierN.cutoffs[songId])) {
             continue;
         }
-
-        const t1List = tier1.cutoffs[songId];
-        const tierList = tierN.cutoffs[songId];
-        const lastT1 = t1List[t1List.length - 1];
-        const lastTier = tierList[tierList.length - 1];
+        if (isVersus && (!vTier1.cutoffs || !vTierN.cutoffs)) {
+            continue;
+        }
+        let t1List = isVersus?vTier1.cutoffs:tier1.cutoffs[songId];
+        let tierList = isVersus?vTierN.cutoffs:tierN.cutoffs[songId];
+        let lastT1 = t1List[t1List.length - 1];
+        let lastTier = tierList[tierList.length - 1];
 
         t1Score.set(Number(songId), lastT1.ep);
         tierScore.set(Number(songId), lastTier.ep);
@@ -86,8 +104,9 @@ export async function drawCutoffSongsDetail(eventId: number, tier: number, mainS
         ? changeTimePeriodFormat(Math.max(0, event.endAt[mainServer] - now), false)
         : '未知';
     const latestTimeStamp = Math.max(...Array.from(latestUpdateTime.values(), v => v || 0));
+    //console.log(now,latestTimeStamp,changeTimePeriodFormat(now - latestTimeStamp, true))
     const updateText = latestTimeStamp > 0
-        ? `${changeTimePeriodFormat(now - latestTimeStamp, false)}前`
+        ? `${changeTimePeriodFormat(now - latestTimeStamp, true)}前`
         : '未知';
 
     list.push(drawListMerge([
