cc9a429d43085fcea28ba782430939ecb8d92556
Kudryavka <Kudryavka03@outlook.com>
2026-07-07 13:19:28 +0800

    fix debug flags

--- src/view/cutoffDetail.ts
+++ src/view/cutoffDetail.ts
@@ -557,7 +557,7 @@ export interface TimePresentEP {
 // 取得特定百分比时候的EP
 export function getTimePresentEP(cutoff:Cutoff,present:number,ratio:number,cutoff2:Cutoff){    // 接受cutoff数据，百分比，比值
     console.log(present)
-      let debugflags = true
+      let debugflags = false
     if (!cutoff || cutoff.cutoffs.length < 1){
         return {value:0,valueWithRatio:0,time:cutoff.endAt} as TimePresentEP
     }
